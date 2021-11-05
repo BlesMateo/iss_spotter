8fcc46ada6d98cd697c065aec106baa7a51e9b48
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-04 23:38:36 -0400

    Commented out all functions except for fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 */
-
+/*
 const { fetchCoordsByIP } = require('./iss');
 
 fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
@@ -23,4 +23,24 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+*/
+const {fetchISSFlyOverTimes} = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+  /*It worked! Returned flyover times: [
+  { risetime: 1636162523, duration: 158 },
+  { risetime: 1636198923, duration: 571 },
+  { risetime: 1636235323, duration: 288 },
+  { risetime: 1636271723, duration: 546 },
+  { risetime: 1636308123, duration: 186 }
+  */
 });
