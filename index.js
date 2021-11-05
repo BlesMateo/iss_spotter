794b1dc3d12c73dc46de17b0c57aa243ec028488
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-05 15:58:46 -0400

    Added a nextISSTimesForMyLocation and printPassTimes functions

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   console.log('It worked! Returned coordinates:' , coordinates);
 });
 */
+
+/*
 const {fetchISSFlyOverTimes} = require('./iss');
 
 const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
@@ -34,13 +36,32 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned flyover times:' , passTimes);
+  */
   /*It worked! Returned flyover times: [
   { risetime: 1636162523, duration: 158 },
   { risetime: 1636198923, duration: 571 },
   { risetime: 1636235323, duration: 288 },
   { risetime: 1636271723, duration: 546 },
   { risetime: 1636308123, duration: 186 }
-  */
+
+});
+*/
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
 });
